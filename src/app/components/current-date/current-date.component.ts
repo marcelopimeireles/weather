43c7d980b4aff2462054dfa32aa1fd3c2f88476e
Marcelo Meireles <marcelomeireles@MBP-de-Marcelo.lan>
2019-12-05 03:21:35 +0000

    current date exibition module

---
+++ src/app/components/current-date/current-date.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { format } from 'date-fns'
+
+@Component({
+  selector: 'app-current-date',
+  templateUrl: './current-date.component.html',
+  styleUrls: ['./current-date.component.scss']
+})
+export class CurrentDateComponent {
+  date: Date;
+  calendar: Array<string>;
+
+  constructor() {
+    this.date = new Date();
+    this.calendar = format(this.date, `iii dd MMM yy '@' HH a`).split(' ');
+  }
+}
