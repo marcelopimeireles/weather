4be9b08bc89598c948d2f2e949d52f74f3aab72f
Marcelo Meireles <marcelomeireles@MBP-de-Marcelo.lan>
2019-12-05 03:23:00 +0000

    app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-import { AppRoutingModule } from './app-routing.module';
+import { FormsModule } from '@angular/forms';
+import { NgbModule, NgbCheckBox } from '@ng-bootstrap/ng-bootstrap';
 import { AppComponent } from './app.component';
+import { ScaleComponent } from './components/scale/scale.component';
+import { CurrentDateComponent } from './components/current-date/current-date.component';
+import { DayBackgroundComponent } from './components/day-background/day-background.component';
+import { CitiesPanelComponent } from './components/cities-panel/cities-panel.component';
+import { ForecastCardComponent } from './components/forecast-card/forecast-card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ScaleComponent,
+    CurrentDateComponent,
+    DayBackgroundComponent,
+    CitiesPanelComponent,
+    ForecastCardComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    NgbModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [NgbCheckBox],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
