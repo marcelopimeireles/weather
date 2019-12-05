0916c4ce16b9f08bc7b92be20d29dfd400193c5d
Marcelo Meireles <marcelomeireles@MBP-de-Marcelo.lan>
2019-12-05 03:19:13 +0000

    app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { format,  } from 'date-fns'
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'weather';
+  title: string;
+  public date: Date;
+  public formattedDate: string;
+  public isDay: boolean;
+
+
+  constructor(){
+    this.title = 'W€ATHER';
+    this.date = new Date();
+    this.formattedDate = format(this.date, 'PPPP');
+    this.isDay = this.checkIfIsDay(this.date);
+  }
+
+  private checkIfIsDay (date) {
+    const [hour, period] = format(date, 'HH a').split(" ");
+    const morning = (Number(hour) >= 6 && period === 'AM');
+    const afternoon = (Number(hour) <= 6 && period === 'PM');
+    return (morning || afternoon);
+  }
 }
